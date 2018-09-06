1dd88fd735629d77ddc56249f53d2206110ecfdc
hanwjdgh <hanwjdgh@naver.com>
2018-09-06 15:35:59 +0900

    get geolocation & weather api

--- weather/App.js
+++ weather/App.js
@@ -1,23 +1,53 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import Weather from "./Weather"
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
+
+const API_KEY = "c2f671578a1618551299f58e730504ce";
 
 export default class App extends React.Component {
-  state={
-    isLoaded:true
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+        console.log(error)
+        error: error
+      }
+    );
   }
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+      });
+  };
+
   render() {
-    const {isLoaded} = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
-      <StatusBar hidden = {true}/>
-        { isLoaded ? (
+        <StatusBar hidden={true} />
+        {isLoaded ? (
           <Weather />
-        ) :(
-        <View style={styles.loading}>
-          <Text style={styles.loadingText}>Getting the weather</Text>
-        </View>
-      )}
+        ) : (
+            <View style={styles.loading}>
+              <Text style={styles.loadingText}>Getting the weather</Text>
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </View>
+          )}
       </View>
     );
   }
@@ -26,16 +56,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
-  loading:{
-    flex:1,
-    backgroundColor:'#FDF6AA',
-    justifyContent:"flex-end",
-    paddingLeft : 25
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    paddingLeft: 40
   },
-  loadingText:{
-    fontSize:38,
-    marginBottom : 100
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
   }
 });
--- weather/Weather.js
+++ weather/Weather.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {LinearGradient} from "expo";
-import {Ionicons} from "@expo/vector-icons"
+import { LinearGradient } from "expo";
+import { Ionicons } from "@expo/vector-icons"
 
-export default class Weather extends Component{
-    render(){
-        return(
-            <LinearGradient 
-                colors={["#00C6FB","#005BEA"]} 
-                style={styles.container} 
+export default class Weather extends Component {
+    render() {
+        return (
+            <LinearGradient
+                colors={["#00C6FB", "#005BEA"]}
+                style={styles.container}
             >
                 <View style={styles.upper}>
-                    <Ionicons color="white" size={144} name="ios-rainy"/>
+                    <Ionicons color="white" size={144} name="ios-rainy" />
                     <Text style={styles.temp}>35°</Text>
                 </View>
                 <View style={styles.lower}>
@@ -25,33 +25,33 @@ export default class Weather extends Component{
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1
+        flex: 1
     },
-    upper:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center"
+    upper: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
     },
-    temp:{
-        fontSize:38,
-        color:"white",
-        marginTop:10
+    temp: {
+        fontSize: 38,
+        color: "white",
+        marginTop: 10
     },
-    lower:{
-        flex:1,
-        alignItems:"flex-start",
-        justifyContent:"flex-end",
-        paddingLeft:25
+    lower: {
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-end",
+        paddingLeft: 25
     },
-    title:{
-        fontSize:38,
-        color:"white",
-        marginBottom:10,
-        fontWeight:"300"
+    title: {
+        fontSize: 38,
+        color: "white",
+        marginBottom: 10,
+        fontWeight: "300"
     },
-    subtitle:{
-        fontSize:24,
-        color:"white",
-        marginBottom:24
+    subtitle: {
+        fontSize: 24,
+        color: "white",
+        marginBottom: 24
     }
 });
