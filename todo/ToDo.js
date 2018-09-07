d5da3fd9232cb560dd7ad1e3ce6c56d1e2ae610b
hanwjdgh <hanwjdgh@naver.com>
2018-09-08 07:53:38 +0900

    Second UI & ToDo.js

---
+++ todo/ToDo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+
+export default class ToDo extends Component{
+    render(){
+        return(
+            <View>
+                <Text>hi</Text>
+            </View>
+        );
+    }
+}
+
+const Styles = StyleSheet.create({
+
+});
